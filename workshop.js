3a33d67b07ca4b36c16079c6a6ef3056ba1663c5
Matt Szabo <mattidj@gmail.com>
2017-07-07 23:24:32 +0000

    added getIssPosition,getAddressPosition,getCurrentTemperatureAtPosition

--- workshop.js
+++ workshop.js
@@ -1,27 +1,88 @@
 var request = require('request-promise');
 
-// Euclidian distance between two points
-function getDistance(pos1, pos2) {
-  return Math.sqrt(Math.pow(pos1.lat - pos2.lat, 2) + Math.pow(pos1.lng - pos2.lng, 2));
+function getIssPosition() {
+  return request('http://api.open-notify.org/iss-now.json')
+  .then(
+    function(response) {
+      // Parse as JSON
+      //console.log(response);
+      var issResObject = JSON.parse(response)
+      var issLatLongObj = {};
+      
+      issLatLongObj.lat = issResObject.iss_position.latitude;
+      issLatLongObj.lng = issResObject.iss_position.longitude;
+      
+      // Return object with lat and lng
+      //console.log("The latitude is: " + issLatLongObj.lat + " & the longitude is: " + issLatLongObj.lng);
+return issLatLongObj;
+    }
+  )
+  
 }
 
-function getIssPosition() {
-  return request(/* ... */)
+getIssPosition()
+.catch(function(err){
+    console.log(err, "iss error")
+})
+
+var address="2212 des Migrations, Montreal, Quebec";
+
+function getAddressPosition(address) {
+
+var key = 'AIzaSyCC0gdpgFjdOUEDcYnAKOeUwTdeG0wQxGg';
+var geo_loc = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + address + "&key=" + key;
+
+  return request(geo_loc)
   .then(
-    function(responce) {
+    function(response) {
       // Parse as JSON
-      // Return object with lat and lng
+      //console.log(response);
+      var googleResponse = JSON.parse(response);
+      var googleObj = {};
+      
+      googleObj.lat = googleResponse.results[0].geometry.location.lat;
+      googleObj.lng = googleResponse.results[0].geometry.location.lng;
+      
+      //console.log("The latitude is: " + googleObj.lat + " & the longitude is: " + googleObj.lng);
+      return googleObj
+    }
+  )
+}
+  
+getAddressPosition(address)
+.catch(function(err){
+    console.log(err, "Google error")
+})
+
+// Complete the code of the function. The position parameter is an object with lat and lng.
+// Make sure your function only returns a Promise for the current temperature (a number) and nothing else
+
+function getCurrentTemperatureAtPosition(address) {
+
+var latlng = 'https://api.darksky.net/forecast/ce049a5702c47003b68c6125f91c05a1/'+address.lat+','+address.lng;
+//console.log(address, "address")
+return request(latlng)
+  .then(function(response) {
+      // Parse as JSON
+      var dsResponse = JSON.parse(response);
+      var dsObj = {};
+      
+      dsObj.currTemp = dsResponse.currently.temperature;
+     console.log("Current temp is: " + dsObj.currTemp);
     }
   )
 }
 
-function getAddressPosition(address) {
-
-}
-
-function getCurrentTemperatureAtPosition(position) {
-
-}
+getAddressPosition(address)
+.then(function(latLng){
+  getCurrentTemperatureAtPosition(latLng)
+})
+//.then(function(data){
+  //then handle data
+//})
+.catch(function(err){
+  console.log(err)
+})
 
 function getCurrentTemperature(address) {
 
@@ -29,4 +90,10 @@ function getCurrentTemperature(address) {
 
 function getDistanceFromIss(address) {
 
+}
+
+
+// Euclidian distance between two points
+function getDistance(pos1, pos2) {
+  return Math.sqrt(Math.pow(pos1.lat - pos2.lat, 2) + Math.pow(pos1.lng - pos2.lng, 2));
 }
