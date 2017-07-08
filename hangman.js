e3db2c4b5f21a2b20f84bce36002ee9119ed296c
Matt Szabo <mattidj@gmail.com>
2017-07-08 20:24:42 +0000

    finished a basic hangman game using an API word

--- hangman.js
+++ hangman.js
@@ -1,60 +1,63 @@
 var inquirer = require('inquirer');
-var wordsurl = "http://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=false&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
+var request = require('request-promise');
+
+
+//var wordsurl = "http://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=false&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
 var chances=7;
-var word="zipper";
+
 var replace = [];
 
+//var p = Promise.resolve();
+var globalword = 'a';
+
 function getWord() {
-  return request(wordsurl)
-  .then(
-    function(response) {
+  return request('http://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=false&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5')
+  .then(function(response) {
       // Parse as JSON
       //console.log(response);
-      var wordObject = JSON.parse(response)
+      var wordObject = JSON.parse(response);
       var returnedWordObj = {};
       
       returnedWordObj.word = wordObject.word;
-  console.log(returnedWordObj.word);
-      // Return object with lat and lng
-      //console.log("The latitude is: " + issLatLongObj.lat + " & the longitude is: " + issLatLongObj.lng);
-return returnedWordObj;
-    }
-  )
-  
-}
-
-getWord().catch(function(err){
-    console.log(err, "word error")
-})
-
-
-
-for (var i = 0;i<word.length;i++){
+      console.log(returnedWordObj.word);
+      globalword = returnedWordObj.word;
+      
+      
+    
+for (var i = 0;i<globalword.length;i++){
   replace.push("*");
 }
 
 
 function Questions(){
+var hits= 0;
+  if (hits >= globalword.length)
+  {
+    console.log("You Win!");  
+    throw new RangeError("Goodbye!");
+  }
 
 inquirer.prompt({
 message: 'Welcome to HANGMAN. Pleaes guess a letter!',
 name: 'guess'
 })
 .then(function(answers) {
-  var hits=0;
-  var guess = parseInt(answers.guess);
-  var n = word.search(answers.guess); 
+  var hits;
+  //var guess = parseInt(answers.guess);
+  var n = globalword.search(answers.guess); 
   
-      for (var i = 0;i<word.length;i++){
-          if (word[i]==answers.guess){
+      for (var i = 0;i<globalword.length;i++){
+          if (globalword[i]==answers.guess){
           replace[i]=answers.guess;
           }
       }
-  
+
    
-  if (chances <= 0)
+  if (chances <= 0 && n<0)
   {
-    console.log("Too bad - game over");
+    console.log("Too bad - game over.");
+    
+    throw new RangeError("Goodbye!");
   }
   
   if (chances > 0 && n >= 0) {
@@ -64,17 +67,15 @@ name: 'guess'
     Questions();
   }
   else if(chances > 0 || n < 0 ){
-    console.log("The letter was not found in our magic word. Please try again! " + chances + " chances left.")
+    console.log("The letter was not found in our magic word. Please try again! " + chances + " chances left.");
     chances--;
+    console.log(replace.join(""));
     Questions();
     
   }
 
   
-  else if (hits >= word.length)
-  {
-    console.log("You Win!");  
-  }
+  
   
 })
 .catch(function(error) {
@@ -84,4 +85,77 @@ name: 'guess'
     
 }
 
-Questions()
+Questions();
+      
+      
+      
+      
+    }
+     
+  );
+  
+
+}
+
+getWord() ;
+
+
+
+
+// for (var i = 0;i<word.length;i++){
+//   replace.push("*");
+// }
+
+
+// function Questions(){
+
+// inquirer.prompt({
+// message: 'Welcome to HANGMAN. Pleaes guess a letter!',
+// name: 'guess'
+// })
+// .then(function(answers) {
+//   var hits=0;
+//   var guess = parseInt(answers.guess);
+//   var n = word.search(answers.guess); 
+  
+//       for (var i = 0;i<word.length;i++){
+//           if (word[i]==answers.guess){
+//           replace[i]=answers.guess;
+//           }
+//       }
+  
+   
+//   if (chances <= 0)
+//   {
+//     console.log("Too bad - game over");
+//   }
+  
+//   if (chances > 0 && n >= 0) {
+//     console.log("Good job! Your letter matches " + chances + " chances left.");
+//     console.log(replace.join(""));
+//     hits++;
+//     Questions();
+//   }
+//   else if(chances > 0 || n < 0 ){
+//     console.log("The letter was not found in our magic word. Please try again! " + chances + " chances left.")
+//     chances--;
+//     Questions();
+    
+//   }
+
+  
+//   else if (hits >= word.length)
+//   {
+//     console.log("You Win!");  
+//   }
+  
+// })
+// .catch(function(error) {
+//   console.log(error);
+// });
+    
+    
+// }
+
+// Questions()
+
